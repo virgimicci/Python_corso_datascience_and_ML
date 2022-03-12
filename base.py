a97cd20a6b81d512b2fb07298c12ec7c137f40d1
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-03-13 00:03:27 +0100

    Create base.py

---
+++ base.py
@@ -0,0 +1,10 @@
+# Impostare la working directory
+
+
+import os
+os.getcwd()
+''' vedo quale è la corrente wd
+...C:\\Users\\micci '''
+
+''' cambio la wd '''
+os.chdir("C:\\Users\\micci\\Documents\\Corso python data science\\CODICE - Corso completo di data science in Python\\Sezione 3")
