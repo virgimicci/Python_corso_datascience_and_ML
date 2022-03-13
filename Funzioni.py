6e5891cbfdb580a084c197e07557346bf073ab68
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-03-13 10:55:53 +0100

    Create Funzioni.py

---
+++ Funzioni.py
@@ -0,0 +1,6 @@
+## Funzioni built-in, parte di py e vengono caricate in automatico
+## Funzione user defined, che possiamo costruire
+# utili per applicare una operazione su piu oggetti senza ripetere lo stessocodice piuvolte,
+#permettono diiterare un operazione
+## Funzioni presenti nelle librerie
+
