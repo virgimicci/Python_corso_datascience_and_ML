6268090e7bd3f5601954f2d82c5358d62b8cceff
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-03-13 10:57:16 +0100

    Update base.py

--- base.py
+++ base.py
@@ -8,3 +8,10 @@ os.getcwd()
 
 ''' cambio la wd '''
 os.chdir("C:\\Users\\micci\\Documents\\Corso python data science\\CODICE - Corso completo di data science in Python\\Sezione 3")
+
+
+# Installazione pacchetto
+
+# conda install nome_pacchetto
+# o
+# pip install nome_pacchetto
