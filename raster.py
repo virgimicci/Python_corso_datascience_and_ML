f8d12f9e6c86e8e3988f75c2b18550ea69d8ab0e
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-03-31 14:27:23 +0200

    Create raster.py

---
+++ raster.py
@@ -0,0 +1,9 @@
+conda install rasterio
+import rasterio
+from rasterio import plot
+import os
+
+os.getcwd() # per vedere la wd
+os.chdir("C:\\Users\\micci\\Documents\\Internship\\sen2r_out\\RGB432B") # per cambiarla 
+         
+os.listdir() # per vedere la lista di file nella wd     
