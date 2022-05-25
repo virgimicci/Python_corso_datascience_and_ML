76b9da5a30cc0966cf08fc6e1e25ff75e4363eb0
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-05-25 10:04:00 +0200

    Update Gestione dataset con pandas.py

--- Gestione dataset con pandas.py
+++ Gestione dataset con pandas.py
@@ -23,7 +23,7 @@ df1.describe()
 df.sort_values(by= "Weight")
 
 # estrarre dei case dal data set (slicing)
-df1(2:) # dal secondo inpoi
+df1(2:) # dal secondo in poi
 
 # metodi per slicing .loc (lavora in base all'indice) 
 df1.loc[1]
@@ -59,6 +59,7 @@ df1.sample(2) # se ripeto l'operazione, ridefinendo lo stesso seed, ottengio gli
 ## Numpy permette di creare dataset casuali 
 # oltretutto ci permette di ottenere altri tipi di distribuzione
 # binomiale
+
 pd.DataFrame(np.random.binomial(100, 0.5, (10, 5)))
 
 # poisson
@@ -68,15 +69,18 @@ pd.DataFrame(np.random.poisson(100, (10, 5)))
 pd.DataFrame(np.random.uniform(1, 100,  (10, 5)))
 
 
-## Matplotlib : pacchetto per la creazione di grafici (gia presente in anaconda)
+## Matplotlib : pacchetto per la creazione di grafici (gia presente in anaconda) 
+# (https://matplotlib.org/)
 
 import matplotlib as mlp
 import matplotlib.pyplot as plt
 
+#funzione base .plot
+
 plt.plot([5,7,2,4]) # grafico con linea di defoult
 plt.plot([5,7,2,4], [4,6,9,2], 'ro') # ro (round obj) inserisce pallini e non linea
 
-# creiamo due oggetti
+# creiamo due liste
 
 x = [50, 70, 90, 65]
 y = [129, 192, 163, 172]
@@ -92,9 +96,38 @@ plt.plot(x, y, color = "yellow")
 plt.title("TITLE", color = "blue")
 plt.xlabel("Asse X", color = "purple")
 plt.ylabel("Asse Y", color = "green")
+plt.grid(True) # se voglio la griglia sotto
+plt.legend(["Legend"], loc= 2) # per aggiungere la legenda, "loc" serve per la posizione
+
+plt.subplot[2,2,1] # sottografici, i num tra parentesi indicano numero di righe, di colonne e num del grafico
+plt.plot([1,2,3,4],[12,4,15,6],"a")
+plt.subplot[2,2,2] # sottografici
+plt.plot([1,3,6,9],[11,9,19,3],"b")
 
 # altri grafici
 plt.pie()
 plt.scatter()
 plt.bar()
+plt.barh()
+plt.area()
+plt.hist()
+plt.boxplot()
+# posso anche scrivere df1.plot(kind = "hist")
+
+plt.savefig("graph1.png", dpi= 600) # per salvare l'immagine nella wd
+
+# Applicare uno stile
+#stili disponibili
+plt.style.available
+
+# come applicarli?
+plt.style.use("ggplot")
+df1.plot(kind = "area")
+
+
+
+
+
+
+
 
